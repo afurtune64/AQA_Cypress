77e1f4c4537c69ecbe8f6c87584c80bb8f7fc82c
afurtune64 <afurtune@64labs.com>
2025-05-27 19:30:28 +0300

    Remove deprecated Cypress example tests and add new queries for the to-do app functionality

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: "https://guest:welcome2qauto@qauto.forstudy.space/",
+    video: true,
+    projectId: "vhjes7",
   },
 });
